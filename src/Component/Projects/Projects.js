1c3877b954f03288a245a301afc823a846603fb0
geertza <geertza@gmail.com>
2020-09-19 05:35:44 -0600

    styled projects

---
+++ src/Component/Projects/Projects.js
@@ -0,0 +1,45 @@
+import React  from 'react';
+import {Figure, Row,Container,Col,Button} from 'react-bootstrap';
+import FigureCaption from 'react-bootstrap/FigureCaption'
+import './projects.css'
+import Nerd from '../../visuals/nerdverse.png'
+class Intro extends React.Component {
+    render() {
+      return (
+          <div className='projects '>
+                <h1  className='Protitle tShadow '>Projects</h1>
+              <Container >
+                    <Row>
+                        <Col sm={5} className='ProCluster' >
+                        <Figure>
+                                <img src={Nerd} alt="" className='proImage roCluster' />
+                                <FigureCaption>
+                                    <h5 className="tShadow">Nerd Verse</h5>
+                                    <span><h5 className="tShadow">Basic Javascript</h5></span>
+                                    <Button variant="warning" className='probutton'>Take a look</Button>
+                                </FigureCaption>
+                            </Figure>
+                        </Col>
+                        <Col sm={2}>
+                        </Col>
+                        <Col sm={5} className='ProCluster'>
+                            
+                        <Figure>
+                                <img src={Nerd} alt="" className='proImage' />
+                                <FigureCaption>
+                                    <h5 className="tShadow">Nerd Verse</h5>
+                                    <span><h5 className="tShadow">Basic Javascript</h5></span>
+                                    <Button variant="warning" className='probutton'>Take a look</Button>
+                                    
+                                </FigureCaption>
+                            </Figure>
+                        </Col>
+                    </Row>
+                   </Container>                         
+                       
+                    </div>
+      )
+    }
+}
+
+export default Intro;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 
 import About from './Component/Intro.js/Intro'
 import Header from './Component/Header/Header'
+import Project from './Component/Projects/Projects'
 ReactDOM.render(
   <React.Fragment>
     <Header />
     <About />
+    <Project />
     {/* <Footer /> */}
     </React.Fragment>,
   document.getElementById('root')
